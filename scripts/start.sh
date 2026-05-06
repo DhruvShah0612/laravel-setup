1c4e756327e7bb6a828819fb4b0280b1fb522350
Ubuntu <ubuntu@ip-172-31-11-152.ap-south-1.compute.internal>
2026-05-06 05:42:47 +0000

    script chnage

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
-
-
 #!/bin/bash
 
-sudo systemctl restart apache2
+cd /var/www/html/laravel
 
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+sudo systemctl restart apache2
