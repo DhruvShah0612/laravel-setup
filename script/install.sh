86b3c6aff182b9b84a1f6e4c6cf8ebe85f2f87b5
Ubuntu <ubuntu@ip-172-31-11-152.ap-south-1.compute.internal>
2026-05-06 05:32:35 +0000

    delete

--- script/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html/laravel
-
-composer install --no-dev --optimize-autoloader
-cp .env.example .env || true
-php artisan key:generate || true
-php artisan migrate --force || true
--- script/start.sh
+++
@@ -1,9 +0,0 @@
-
-
-#!/bin/bash
-
-sudo systemctl restart apache2
-
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
